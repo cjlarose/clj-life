e43aed26593ce16b248b0dfecf1fda5098759188
Chris LaRose <chris.larose@mycase.com>
2015-08-18 15:14:38 -0700

    cjl - Printing

--- src/life/core.clj
+++ src/life/core.clj
@@ -1,4 +1,5 @@
 (ns life.core
+  (:require [clojure.string :refer [join]])
   (:gen-class))
 
 (def glider
@@ -18,20 +19,30 @@
                  :when (in-bounds? neighbor)]
              neighbor))))
 
+(defn coords [{:keys [height width]}]
+  (for [i (range height)
+        j (range width)]
+    [i j]))
+
 (defn next-grid [{:keys [height width cells] :as grid}]
-  (let [new-cells (for [i (range height)
-                        j (range width)
-                        :let [cell [i j]]
-                        :when (let [n (num-neighbors grid cell)]
-                                (if (cells cell)
-                                  (and (> n 1) (< n 3))
-                                  (= n 3)))]
-                   cell)]
-    (assoc grid :cells (set new-cells))))
+  (let [should-live? (fn [cell]
+                       (let [n (num-neighbors grid cell)]
+                         (if (cells cell)
+                           (and (> n 1) (< n 3))
+                           (= n 3))))]
+    (->> (coords grid)
+         (filter should-live?)
+         (set)
+         (assoc grid :cells))))
 
 (defn all-grids [grid]
   (let [living? (fn [{:keys [cells]}] (not (empty? cells)))]
     (take-while living? (iterate next-grid grid))))
+
+(defn grid->str [{:keys [height width cells]}]
+  (let [cell->str (fn [cell] (if (cells cell) "o" "x"))
+        row->str (fn [i] (apply str (map cell->str (map (partial conj [i]) (range width)))))]
+  (join "\n" (map row->str (range height)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -40,4 +51,5 @@
         future-grids (all-grids initial-grid)]
     (doseq [grid future-grids]
       (println grid)
+      (println (grid->str grid))
       (Thread/sleep 100))))
